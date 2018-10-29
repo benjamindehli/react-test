b334f3f25732bbab37421310048bdf2465d492f0
Benjamin Dehli <benjamin@arkitektum.no>
2018-10-29 16:56:31 +0100

    Add basic component for list

---
+++ src/components/List.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+import './List.scss';
+
+class ListItem extends React.Component {
+	render() {
+		return (
+			<li>{ this.props.listItem }</li>
+		);
+	}
+}
+
+class List extends React.Component {
+	renderList() {
+		let listItems = this.props.listItems.map(function(listItem, i){
+			return <ListItem listItem={listItem} key={i}/>;
+		});
+		return <ul className="list">{listItems}</ul>;
+	}
+	render () {
+		return (
+			<div>
+				{this.renderList()}
+			</div>
+			)
+	}
+
+}
+List.propTypes = {
+	listItems: PropTypes.array
+}
+
+List.defaultProps = {
+	listItems: ['testing123']
+}
+
+export default List;
+
+
