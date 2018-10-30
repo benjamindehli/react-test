4b79037d0013fa4efd741fcbf537a7b02cf4beff
Benjamin Dehli <benjamin@arkitektum.no>
2018-10-30 13:29:12 +0100

    Use Open Sans font

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import List from './lib/components/List';
 import Button from './lib/components/Button';
 import Colors from './lib/components/Colors';
 
+WebFont.load({
+  google: {
+    families: ['Open Sans:300,400,700', 'sans-serif']
+  }
+});
 
 ReactDOM.render(
   <List listItems={['item 1', 'item 2']} />,
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,12 @@
 'use strict';
+import WebFont from 'webfontloader';
 import Button from './components/Button';
 import List from './components/List';
+
+WebFont.load({
+  google: {
+    families: ['Open Sans:300,400,700', 'sans-serif']
+  }
+});
 
 export {List, Button}
