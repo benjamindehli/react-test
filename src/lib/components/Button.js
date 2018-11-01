9cf2b931374c70069ac9b2f0f9632098816cd88b
Benjamin Dehli <benjamin@arkitektum.no>
2018-11-01 19:06:16 +0100

    Add support for small buttons

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -4,7 +4,9 @@ import style from './Button.scss';
 
 class Button extends React.Component {
 	render () {
-		let className = style.button + " " + style[this.props.color];
+		let colorClass = " " + style[this.props.color];
+		let sizeClass = " " + style[this.props.size];
+		let className = style.button + colorClass + sizeClass;
 		return (
 			<button className={className}>{this.props.content}</button>
 			)
@@ -15,11 +17,13 @@ Button.propTypes = {
 	/** Text content inside button */
 	content: PropTypes.string,
 	color: PropTypes.oneOf(['default', 'primary', 'success', 'warning']),
+	size: PropTypes.oneOf(['small', 'regular'])
 }
 
 Button.defaultProps = {
 	content: 'button',
-	color: 'default'
+	color: 'default',
+	size: 'regular'
 }
 
 export default Button;
--- src/lib/components/Button.md
+++ src/lib/components/Button.md
@@ -1,4 +1,4 @@
-Button example:
+Regular buttons example:
 
 ```js
 <Button content="Click me" color="default" />
@@ -6,4 +6,14 @@ Button example:
 <Button content="Click me" color="info" />
 <Button content="Click me" color="warning" />
 <Button content="Click me" color="success" />
+```
+
+Small buttons example:
+
+```js
+<Button content="Click me" color="default" size="small" />
+<Button content="Click me" color="primary" size="small" />
+<Button content="Click me" color="info" size="small" />
+<Button content="Click me" color="warning" size="small" />
+<Button content="Click me" color="success" size="small" />
 ```
