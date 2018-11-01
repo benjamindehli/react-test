1bf28dc29b131bee07f00c5b1f51d1fa53c904f8
Benjamin Dehli <benjamin@arkitektum.no>
2018-11-01 15:29:19 +0100

    Build package

---
+++ dist/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './Header.scss';
+
+class Header extends React.Component {
+  render() {
+    let headerElement = React.createElement('h' + this.props.size, {
+      className: style.header
+    }, this.props.content);
+    return React.createElement("div", {
+      className: style.headerContainer
+    }, " ", headerElement, " ");
+  }
+
+}
+
+Header.propTypes = {
+  /** Text content inside button */
+  content: PropTypes.string.isRequired,
+  size: PropTypes.oneOf([1, 2, 3, 4, 5])
+};
+Header.defaultProps = {
+  content: '',
+  size: 1
+};
+export default Header;
--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,14 @@
 
 import WebFont from 'webfontloader';
 import Button from './components/Button';
+import Header from './components/Header';
 import List from './components/List';
+import LoadingAnimation from './components/LoadingAnimation';
 import NavigationBar from './components/NavigationBar';
+import Select from './components/Select';
 WebFont.load({
   google: {
     families: ['Open Sans:300,400,700', 'sans-serif']
   }
 });
-export { Button, List, NavigationBar };
+export { Button, Header, List, LoadingAnimation, NavigationBar, Select };
