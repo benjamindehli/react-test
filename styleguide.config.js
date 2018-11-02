d545aa81ade09f86815b127f5c89460ecbd49d7a
Benjamin Dehli <benjamin@arkitektum.no>
2018-11-02 13:51:20 +0100

    Fix HMR for style guide

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 module.exports = {
 	title: "DIBK design",
 	styleguideDir: "docs",
+	contextDependencies: [path.resolve(__dirname, 'src/lib/components')],
 	require: [
 		path.join(__dirname, 'src/styleguide/styleguide.js')
 	],
