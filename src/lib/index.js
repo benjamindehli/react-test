baa8bf0f6ef4dc40793db2e0a774cf6691219a1a
Benjamin Dehli <benjamin@arkitektum.no>
2019-10-17 12:52:49 +0200

    Adjust object for WebFont load function

--- src/lib/index.js
+++ src/lib/index.js
@@ -10,7 +10,7 @@ import Select from './components/Select';
 
 WebFont.load({
   google: {
-    families: ['Changa:200,300,400|Open Sans:300,400,700', 'sans-serif']
+    families: ['Changa:200,300,400', 'Open Sans:300,400,700', 'sans-serif']
   }
 });
 
