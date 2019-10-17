b83cfeaa66d01d4bd84a5c0b57f50b11d5fb6083
Benjamin Dehli <benjamin@arkitektum.no>
2019-10-17 12:53:24 +0200

    Build package

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ import NavigationBar from './components/NavigationBar';
 import Select from './components/Select';
 WebFont.load({
   google: {
-    families: ['Changa:200,300,400|Open Sans:300,400,700', 'sans-serif']
+    families: ['Changa:200,300,400', 'Open Sans:300,400,700', 'sans-serif']
   }
 });
 export { Button, ContentBox, Header, List, LoadingAnimation, NavigationBar, Select };
