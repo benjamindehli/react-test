807cb6987b6674ca08b518f9d7b125b7e90e830c
Benjamin Dehli <benjamin@arkitektum.no>
2019-10-22 09:03:29 +0200

    Use footer as node element type in footer component

--- src/lib/components/Footer.js
+++ src/lib/components/Footer.js
@@ -3,11 +3,11 @@ import style from './Footer.module.scss';
 
 class Footer extends React.Component {
   render() {
-    return (<div className={style.footer}>
+    return (<footer className={style.footer}>
       <div className={style.footerContainer}>
         {this.props.children}
       </div>
-    </div>)
+    </footer>)
   }
 }
 
