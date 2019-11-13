241fd9a2e40d3a07fc7a816f3c5cae87e924e71c
Benjamin Dehli <benjamin@arkitektum.no>
2019-11-13 16:02:25 +0100

    Adjust logic and style rules for select lists

--- src/lib/components/Select.js
+++ src/lib/components/Select.js
@@ -2,57 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './Select.module.scss';
 
-class Option extends React.Component {
-	render() {
-		if (typeof(this.props.option) == 'string'){
-			return (
-				<option value={this.props.option}>{ this.props.option }</option>
-				)
-		}else if (typeof(this.props.option) == 'object'){
-			return (
-				<option value={this.props.option.value}>{ this.props.option.key }</option>
-				)
-		}else {
-			return null;
-		}
-	}
-}
-
 class Select extends React.Component {
-	renderList() {
-		let options = this.props.options.map(function(option, i){
-			return <Option option={option} key={i}/>;
-		});
-		return <select>{options}</select>;
-	}
-	render () {
-		return (
-			<div className={ style.selectContainer }>
-				{this.renderList()}
-			</div>
-			)
-	}
+  renderOptionElements(options) {
+    return options.map((option, key) => {
+      let optionObject = null;
+      let isSelected = false;
+      if (typeof(option) === 'object') {
+        optionObject = {
+          key: option.key
+            ? option.key
+            : '',
+          value: option.value
+            ? option.value
+            : ''
+        };
+        isSelected = this.props.value && option.value && this.props.value === option.value;
+      } else {
+        optionObject = {
+          key: option,
+          value: option
+        }
+        isSelected = this.props.value && option && this.props.value === option;
+      }
+      return <option value={optionObject.value} key={key} selected={isSelected}>{optionObject.key}</option>
+    });
+  }
+  render() {
+    return (<div className={style.selectContainer}>
+      <label htmlFor={this.props.id}>{this.props.label}</label>
+      <select {...this.props} id={this.props.id}>
+        {this.renderOptionElements(this.props.options)}
+      </select>
+    </div>)
+  }
 }
-
 
 Select.propTypes = {
-	options: PropTypes.arrayOf(
-		PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.shape({
-				key: PropTypes.string,
-				value: PropTypes.string
-			})
-		])
-	),
-	multiple: PropTypes.bool
-}
-
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  multiple: PropTypes.bool,
+  options: PropTypes.arrayOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({key: PropTypes.string, value: PropTypes.string})
+  ])),
+  value: PropTypes.any,
+  label: PropTypes.string
+};
 Select.defaultProps = {
-	options: [],
-	multiple: false
-}
-
+  name: '',
+  options: [],
+  label: ''
+};
 export default Select;
-
-export {Select, Option};
