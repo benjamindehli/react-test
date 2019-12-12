9c2a5e960535ee2cf495576e43b82e2f51a22974
Benjamin Dehli <superelg@gmail.com>
2019-12-12 11:02:24 +0100

    Build package

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -1,3 +1,5 @@
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './Button.module.scss';
@@ -7,9 +9,9 @@ class Button extends React.Component {
     let colorClass = " " + style[this.props.color];
     let sizeClass = " " + style[this.props.size];
     let className = style.button + colorClass + sizeClass;
-    return React.createElement("button", {
+    return React.createElement("button", _extends({}, this.props, {
       className: className
-    }, this.props.content);
+    }), this.props.content);
   }
 
 }
