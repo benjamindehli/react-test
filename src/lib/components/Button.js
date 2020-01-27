440645fc709b80b8b4151d53aa4e097705ac39ee
Benjamin Dehli <superelg@gmail.com>
2020-01-27 14:23:40 +0100

    Add logic and style rules for buttons with arrows and disabled buttons

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -3,27 +3,35 @@ import PropTypes from 'prop-types';
 import style from './Button.module.scss';
 
 class Button extends React.Component {
-	render () {
-		let colorClass = " " + style[this.props.color];
-		let sizeClass = " " + style[this.props.size];
-		let className = style.button + colorClass + sizeClass;
-		return (
-			<button {...this.props} className={className}>{this.props.content}</button>
-			)
-	}
+
+  getArrowClass() {
+    switch (this.props.arrow) {
+      case 'left':
+        return style.hasArrowLeft;
+      case 'right':
+        return style.hasArrowRight
+      default:
+        return ''
+    }
+  }
+  render() {
+    const className = `${style.button} ${style[this.props.color]} ${style[this.props.size]} ${this.getArrowClass()}`;
+    return (<button {...this.props} className={className}>{this.props.content}</button>)
+  }
 }
 
 Button.propTypes = {
-	/** Text content inside button */
-	content: PropTypes.string,
-	color: PropTypes.oneOf(['default', 'primary', 'success', 'warning']),
-	size: PropTypes.oneOf(['small', 'regular'])
+  /** Text content inside button */
+  content: PropTypes.string,
+  color: PropTypes.oneOf(['default', 'primary', 'success', 'warning']),
+  size: PropTypes.oneOf(['small', 'regular']),
+  arrow: PropTypes.oneOf(['left', 'right'])
 }
 
 Button.defaultProps = {
-	content: 'button',
-	color: 'default',
-	size: 'regular'
+  content: 'button',
+  color: 'default',
+  size: 'regular'
 }
 
 export default Button;
