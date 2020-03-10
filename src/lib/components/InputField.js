1ddda3a82d1c6309636d36b8f574fce139b517b9
Benjamin Dehli <superelg@gmail.com>
2020-03-10 16:31:51 +0100

    Add better support for file inputs

--- src/lib/components/InputField.js
+++ src/lib/components/InputField.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from './Button';
 import style from './InputField.module.scss';
 
 class InputField extends React.Component {
   render() {
-    return (<div className={style.inputField}>
-      <label htmlFor={this.props.id}>{this.props.label}</label>
+    return (<div className={`${style.inputField} ${style[this.props.type]}`}>
+      <label htmlFor={this.props.id}>
+        {this.props.label}
+        {
+          this.props.type === 'file'
+            ? (<div className={style.fileInputContainer}>
+                <span className={style.input}>{this.props.selectedFileName}</span>
+                {
+                  this.props.buttonContent
+                    ? <Button size='small' color={this.props.buttonColor} onClick={() => {document.getElementById(this.props.id).click()}} content={this.props.buttonContent}/>
+                    : ''
+                }
+              </div>)
+            : ''
+        }
+      </label>
       {
         !this.props.contentOnly
           ? <input name={this.props.name} readOnly={this.props.readOnly} disabled={this.props.disabled} type={this.props.type} id={this.props.id} onChange={this.props.onChange} value={this.props.value}/>
           : <span>{this.props.value}</span>
       }
-
     </div>)
   }
 }
@@ -30,12 +44,18 @@ InputField.propTypes = {
       PropTypes.object
     ]))
   ]),
-  contentOnly: PropTypes.bool
+  contentOnly: PropTypes.bool,
+  buttonColor: PropTypes.string,
+  buttonContent: PropTypes.string,
+  selectedFileName: PropTypes.string
 };
+
 InputField.defaultProps = {
   name: '',
   type: 'text',
   label: '',
-  contentOnly: false
+  contentOnly: false,
+  buttonColor: 'default',
 };
+
 export default InputField;
