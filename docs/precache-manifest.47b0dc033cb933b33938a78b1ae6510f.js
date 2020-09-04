1bb21f210234bf120157afe25e06548301560ea5
Benjamin Dehli <superelg@gmail.com>
2020-09-04 15:34:25 +0200

    Build style guide

---
+++ docs/precache-manifest.47b0dc033cb933b33938a78b1ae6510f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab200808910ed909490",
+    "url": "build/2.7ab20080.js"
+  },
+  {
+    "revision": "d87dfd2597cb31b06dd220c32daf5f70",
+    "url": "build/2.7ab20080.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cdfe1233f4de2fff2fa",
+    "url": "build/bundle.0cdfe123.js"
+  },
+  {
+    "revision": "1d312846eb773d737425",
+    "url": "build/main.1d312846.js"
+  },
+  {
+    "revision": "03515b6c6803e22f86a1f799abab99aa",
+    "url": "index.html"
+  },
+  {
+    "revision": "7ab200808910ed909490",
+    "url": "static/css/2.2a60accc.chunk.css"
+  },
+  {
+    "revision": "1d312846eb773d737425",
+    "url": "static/css/main.180aa1fe.chunk.css"
+  },
+  {
+    "revision": "c5e7ea2bc5e05baf28f383979863ab6c",
+    "url": "static/media/Altis-Light.c5e7ea2b.ttf"
+  },
+  {
+    "revision": "c95acee647a3f6a515bf8fda3774d84b",
+    "url": "static/media/Altis-Light.c95acee6.woff"
+  },
+  {
+    "revision": "d9a6cd29cd8ddb42e6a0959f1fad729f",
+    "url": "static/media/Altis-Light.d9a6cd29.svg"
+  },
+  {
+    "revision": "f02874c4eeeba26af5d6e091bb8dc41f",
+    "url": "static/media/Altis-Light.f02874c4.eot"
+  },
+  {
+    "revision": "3d8cd6bf510d512fbbf6c0e5332ff755",
+    "url": "static/media/dibk-logo-mobile.3d8cd6bf.svg"
+  },
+  {
+    "revision": "d707bf9b1785c201d1e7fc8520e28e43",
+    "url": "static/media/hamburger-hover.d707bf9b.svg"
+  },
+  {
+    "revision": "3e6caca9ceb36036920de7ea959c49db",
+    "url": "static/media/hamburger.3e6caca9.svg"
+  },
+  {
+    "revision": "4e12ad68b8688ced77ca28a2cb194eea",
+    "url": "static/media/spinner.4e12ad68.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3959067d5f4768a4f83263ebc5e1779d.js"
+  "precache-manifest.47b0dc033cb933b33938a78b1ae6510f.js"
 );
 
 self.addEventListener('message', (event) => {
