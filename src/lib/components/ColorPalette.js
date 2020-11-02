4a34f00dfe4630744d10f9b8a78bd15878a6b4c5
Benjamin Dehli <superelg@gmail.com>
2020-11-02 17:34:29 +0100

    Add color palette component

---
+++ src/lib/components/ColorPalette.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getThemePaletteBackgroundColor, getThemePaletteTextColor } from 'lib/functions/theme';
+import style from './ColorPalette.module.scss';
+
+class ColorPalette extends React.Component {
+  getThemeStyle(theme, color){
+    return {
+      backgroundColor: getThemePaletteBackgroundColor(theme, color),
+      color: getThemePaletteTextColor(theme, color)
+    }
+  }
+
+	renderColors(theme) {
+    const colors = ['default', 'primary', 'success', 'warning', 'cyan', 'lightCyan', 'orange', 'lightOrange', 'lime', 'lightLime'];
+    return colors.map(color => {
+      const colorClassName = style[color];
+      const themeStyle =  this.props.theme ? this.getThemeStyle(this.props.theme, color) : null;
+      return <div className={`${style.color} ${colorClassName}`} style={themeStyle}>{ color }</div>;
+    });
+	}
+
+	render () {
+		return (
+				<div className={style.colorPalette}>
+					{this.renderColors(this.props.theme)}
+				</div>
+				)
+		}
+	}
+
+ColorPalette.propTypes = {
+	theme: PropTypes.object
+}
+
+ColorPalette.defaultProps = {
+	content: '',
+}
+
+export default ColorPalette;
---
+++ src/lib/components/ColorPalette.md
@@ -0,0 +1,14 @@
+ColorPalette:
+
+```js
+<ColorPalette />
+```
+
+
+
+Themed ColorPalette:
+
+```js
+import customTheme from 'data/customTheme';
+<ColorPalette theme={customTheme} />
+```
